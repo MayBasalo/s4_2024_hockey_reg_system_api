7497c0d2a169446fdfd5a6d23778549bf55fe26f
MayBasalo <May.Basalo@keyin.com>
2025-06-17 00:37:13 -0230

    Final

--- src/main/java/com/keyin/rest/player/PlayerService.java
+++ src/main/java/com/keyin/rest/player/PlayerService.java
@@ -1,12 +1,11 @@
 package com.keyin.rest.player;
 
-import com.keyin.rest.division.Division;
-import com.keyin.rest.division.DivisionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.StreamSupport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class PlayerService {
@@ -14,17 +13,16 @@ public class PlayerService {
     private PlayerRepository playerRepository;
 
     public List<Player> getAllPlayers() {
-        return (List<Player>) playerRepository.findAll();
+        return StreamSupport.stream(playerRepository.findAll().spliterator(), false)
+                .toList();
     }
 
     public Player getPlayerById(long id) {
-        Optional<Player> divisionOptional = playerRepository.findById(id);
-
-        return divisionOptional.orElse(null);
+        return playerRepository.findById(id).orElse(null);
     }
 
-    public Player getPlayerByLastName(String lastName) {
-        return playerRepository.findByLastName(lastName);
+    public List<Player> getPlayersByLastName(String lastName) {
+        return playerRepository.findByLastNameIgnoreCase(lastName);
     }
 
     public void deletePlayerById(long id) {
@@ -37,17 +35,13 @@ public class PlayerService {
 
     public Player updatePlayer(long id, Player updatedPlayer) {
         Optional<Player> playerToUpdateOptional = playerRepository.findById(id);
-
         if (playerToUpdateOptional.isPresent()) {
             Player playerToUpdate = playerToUpdateOptional.get();
-
             playerToUpdate.setFirstName(updatedPlayer.getFirstName());
             playerToUpdate.setLastName(updatedPlayer.getLastName());
             playerToUpdate.setBirthday(updatedPlayer.getBirthday());
-
             return playerRepository.save(playerToUpdate);
         }
-
         return null;
     }
 }
