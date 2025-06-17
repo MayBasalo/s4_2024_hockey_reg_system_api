4403e54ef059d0404b63bf4ce148ee7dde751ba3
MayBasalo <May.Basalo@keyin.com>
2025-06-17 01:27:48 -0230

    Update Locally DivisionRepository.java

--- src/main/java/com/keyin/rest/division/DivisionRepository.java
+++ src/main/java/com/keyin/rest/division/DivisionRepository.java
@@ -3,9 +3,11 @@ package com.keyin.rest.division;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DivisionRepository extends CrudRepository<Division, Long> {
-    public Division findByName(String name);
+    Optional<Division> findByName(String name);
 
-    public Division findByStartBirthYear(String startBirthYear);
+    Optional<Division> findByStartBirthYear(String startBirthYear);
 }
